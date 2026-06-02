ee8307af2a6e76066c040fca323b2896c15c6548
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-06-01 21:30:52 -0300

    feat(posts controller) Protegendo a criação de posts por permissão - Importa o Decorator de permissões - Troca a regra deroles na criação por permissão CASL

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -2,18 +2,34 @@ import { Injectable } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CalsAbilityService } from 'src/cals/cals-ability/cals-ability.service';
 
 @Injectable()
 export class PostsService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(
+    private prismaService: PrismaService,
+    private abilityService: CalsAbilityService,
+  ) {}
 
   create(createPostDto: CreatePostDto & { authorId: string }) {
+    const ability = this.abilityService.ability;
+
+    if (!ability.can('create', 'Post')) {
+      throw new Error('Unauthorized');
+    }
+
     return this.prismaService.post.create({
       data: createPostDto,
     });
   }
 
   findAll() {
+    const ability = this.abilityService.ability;
+
+    if (!ability.can('read', 'Post')) {
+      throw new Error('Unauthorized');
+    }
+
     return this.prismaService.post.findMany();
   }
 
