1b74ca33304091b99308648639debf19bca0a2f3
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-06-01 21:32:49 -0300

    feat(users service) Validando permissão de criação de usuários - Injeta o serviço de abilities - Bloqueia criação sem permissão adequada - Mantém a criação via Prisma após a validação

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,12 +3,22 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import bcrypt from 'bcrypt';
+import { CalsAbilityService } from 'src/cals/cals-ability/cals-ability.service';
 
 @Injectable()
 export class UsersService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(
+    private prismaService: PrismaService,
+    private abilityService: CalsAbilityService,
+  ) {}
 
   create(createUserDto: CreateUserDto) {
+    const ability = this.abilityService.ability;
+
+    if (!ability.can('create', 'User')) {
+      throw new Error('Unauthorized');
+    }
+
     return this.prismaService.user.create({
       data: {
         ...createUserDto,
